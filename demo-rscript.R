fce99b2e6c6c972b1cbb91eba029f02628ddd93d
scworland-usgs <scworland@usgs.gov>
2015-08-28 11:38:01 -0500

    first commit

---
+++ demo-rscript.R
@@ -0,0 +1,14 @@
+
+# LMG-R-group demonstration
+
+x <- 1:100 
+y <- x + runif(100,0,30)
+
+fit <- lm(y~x)
+summary(fit)
+
+plot(x,y)
+abline(fit,col="blue")
+
+#library(ggplot2)
+#qplot(x,y) + geom_smooth(method="lm")
