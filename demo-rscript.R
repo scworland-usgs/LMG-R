bcc27004fa49fa615b5423fd37a991990c42f8f9
scworland-usgs <scworland@usgs.gov>
2015-08-28 11:40:52 -0500

    changed plotting

--- demo-rscript.R
+++ demo-rscript.R
@@ -7,8 +7,8 @@ y <- x + runif(100,0,30)
 fit <- lm(y~x)
 summary(fit)
 
-plot(x,y)
-abline(fit,col="blue")
+#plot(x,y)
+#abline(fit,col="blue")
 
-#library(ggplot2)
-#qplot(x,y) + geom_smooth(method="lm")
+library(ggplot2)
+qplot(x,y) + geom_smooth(method="lm")
